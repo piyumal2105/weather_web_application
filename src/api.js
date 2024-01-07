8faae62fdc8e3bbcae64bf6c11e092b9520d42ab
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-01-07 13:40:57 +0530

    Compleate search function in district

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "43a23dc955msh19e6876cc64cc48p153b8bjsn0b3b83145a99",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
